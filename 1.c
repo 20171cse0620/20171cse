0a3c72f7a95dc5ee6071763242bc539d80a141fa
20171cse0620 <60249618+20171cse0620@users.noreply.github.com>
2020-01-24 13:19:29 +0530

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int m,n;
+printf("enter the no of tokens given to teja\n");
+scanf("%d", &n);
+for(m=0; m<n; m--);
+{
+printf("enter the no of tickets remaining\n");
+}
+}
+
