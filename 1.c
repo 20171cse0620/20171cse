9194959e28c2ff9b0cff765f896b93aa707e3a53
20171cse0620 <60249618+20171cse0620@users.noreply.github.com>
2020-01-24 13:47:20 +0530

    Update 1.c

--- 1.c
+++ 1.c
@@ -5,6 +5,7 @@ int m,n,a,s;
 printf("enter the no of tokens given to teja\n");
 scanf("%d", &n);
 printf("enter the name");
+scanf("%d",&a);
 for(m=0; m<n; m--);
 {
 printf("enter the no of tickets remaining\n");
